546e333806d9e7f64a67d499e6d67a235c2aefa6
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-08-23 22:19:51 +0100

    Add files via upload

--- 04 - first_taste_recursion.pl
+++ 04 - first_taste_recursion.pl
@@ -1,13 +1,12 @@
-% Example 03 - Deduction
+% Example 04 - Recursion First Example
 
 % facts
 parent(john, jane).
 parent(john, james).
 parent(sally, jane).
 parent(martha, sally).
+parent(deirdre, martha).
 
 % relations
 ancestor(X,Y) :- parent(X,Y).
 ancestor(X,Y) :- parent(X,Z), ancestor(Z,Y).
-
-sibling(X,Y) :- parent(Z,X), parent(Z,Y), dif(X,Y).
