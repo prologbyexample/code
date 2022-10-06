dab65dc5c079dc60c850438bd750abc956d586f9
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-10-07 00:55:22 +0100

    Add files via upload

--- 09 - recursion_len_list.pl
+++ 09 - recursion_len_list.pl
@@ -1,10 +1,8 @@
 % Example 09 - Recursion With An Accumulator
 
-% recursive definition of reversed
-reverse([],Z,Z).
-reverse([H|T],Z,A) :- reverse(T,Z,[H|A]).
-
-rev(X,Y) :- reverse(X,Y,[]).
-
-step([], L, L).
+% recursive definition
+step([], L2, L2):- !.
 step([H1|T1], X, L2) :- step(T1, X, [H1|L2]).
+
+% convenience property around step/3
+reverse(X, Y) :- step(X, Y, []).
