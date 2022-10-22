c73827cf85b70ff24dd14c04bc4f7017b085600b
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-10-22 23:56:50 +0100

    Add files via upload

---
+++ 09 - reveresed_accumulator.pl
@@ -0,0 +1,8 @@
+% Example 09 - Recursion With An Accumulator
+
+% recursive definition
+step([], L2, L2).
+step([H1|T1], X, L2) :- step(T1, X, [H1|L2]).
+
+% convenience property around step/3
+reversed(X, Y) :- step(X, Y, []).
