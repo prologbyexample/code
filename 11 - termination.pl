767d8872b79ce8b6a601a029e546af9abba5b3a4
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-11-05 22:56:37 +0000

    Add files via upload

--- 11 - termination.pl
+++ 11 - termination.pl
@@ -10,6 +10,6 @@ reversed(X, Y) :- step(X, Y, []).
 % updated to terminate when first list is a variable
 reversed2(X, Y) :- same_length(X,Y), step(X, Y, []).
 
-% optional definition of samlelength/2 if same_length/2 isn't provided
+% samlelength/2 if same_length/2 isn't provided
 samelength([], []).
 samelength([_H1|T1], [_H2|T2]) :- samelength(T1, T2).
