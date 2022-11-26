d59b724f740d96bd0300ed0474256cd5c6235588
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-11-26 23:44:22 +0000

    Add files via upload

--- 14 - cut_for_efficiency.pl
+++ 14 - cut_for_efficiency.pl
@@ -5,10 +5,7 @@ water(Temp, ice) :- Temp =< 0.
 water(Temp, liquid) :- Temp > 0, Temp < 100.
 water(Temp, gas) :- Temp >= 100.
 
-% clpfd finite domain
-:- use_module(library(clpfd)).
-
 % water2/2 relates temperature to state
-water2(Temp, ice) :- Temp #=< 0.
-water2(Temp, liquid) :- Temp #> 0, Temp #< 100.
-water2(Temp, gas) :- Temp #>= 100.
+water2(Temp, ice) :- Temp =< 0, !.
+water2(Temp, liquid) :- Temp > 0, Temp < 100, !.
+water2(Temp, gas) :- Temp >= 100.
