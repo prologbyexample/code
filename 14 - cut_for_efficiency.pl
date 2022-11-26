44912a9b9676c32ce4138f15f7dce77adacb9f5d
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-11-26 23:38:21 +0000

    interim containing future clpfd

--- 14 - cut_for_efficiency.pl
+++ 14 - cut_for_efficiency.pl
@@ -1,5 +1,14 @@
 % Example 14 - The Cut For Efficiency
 
-% max(X, Y, Max) is true if Max is the larger of X and Y
-max(X, Y, Max) :- X > Y, Max=X.
-max(X, Y, Max) :- Y >= X, Max=Y.
+% water/2 relates temperature to state
+water(Temp, ice) :- Temp =< 0.
+water(Temp, liquid) :- Temp > 0, Temp < 100.
+water(Temp, gas) :- Temp >= 100.
+
+% clpfd finite domain
+:- use_module(library(clpfd)).
+
+% water2/2 relates temperature to state
+water2(Temp, ice) :- Temp #=< 0.
+water2(Temp, liquid) :- Temp #> 0, Temp #< 100.
+water2(Temp, gas) :- Temp #>= 100.
