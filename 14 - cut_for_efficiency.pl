9e34959d64bd6a4e1a27b049a2418f09cb63997d
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-12-01 17:24:56 +0000

    Add files via upload

--- 14 - cut_for_efficiency.pl
+++ 14 - cut_for_efficiency.pl
@@ -1,11 +1,11 @@
 % Example 14 - The Cut For Efficiency
 
 % water/2 relates temperature to state
-water(Temp, ice) :- Temp =< 0.
+water(Temp, solid) :- Temp =< 0.
 water(Temp, liquid) :- Temp > 0, Temp < 100.
 water(Temp, gas) :- Temp >= 100.
 
-% water2/2 uses the cut to be more efficient
-water2(Temp, ice) :- Temp =< 0, !.
-water2(Temp, liquid) :- Temp > 0, Temp < 100, !.
-water2(Temp, gas) :- Temp >= 100.
+% h2o/2 uses the cut to be more efficient
+h2o(Temp, solid) :- Temp =< 0, !.
+h2o(Temp, liquid) :- Temp > 0, Temp < 100, !.
+h2o(Temp, gas) :- Temp >= 100. 
