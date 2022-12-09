fb94c7bcb9bcf1211dd26ff0feb8a4802f409591
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-12-09 01:23:25 +0000

    Add files via upload

--- 15 - negation.pl
+++ 15 - negation.pl
@@ -1,9 +1,20 @@
 % Example 15 - Negation By Failure
 
 % fruit
-fruit(apple).
-fruit(orange).
+fruit(apple). 
+fruit(orange). 
 fruit(banana).
+yellow(banana).
 
-% fruit that Mary likes
+% Mary likes all fruit
 likes(mary, X) :- fruit(X).
+
+% James like all fruit, as long as it is yellow
+likes(james, X) :- fruit(X), yellow(X).
+
+% John likes all fruit, except yellow fruit
+likes(jo, X) :- fruit(X), neg(yellow(X)).
+
+% negation as failure
+neg(G) :- call(G), !, fail.
+neg(_).
