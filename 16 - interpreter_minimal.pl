61b0a92f379d4cfa462cb46c67626df135fdb419
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-12-23 02:38:57 +0000

    Delete 16 - interpreter_minimal.pl

--- 16 - interpreter_minimal.pl
+++
@@ -1,9 +0,0 @@
-% Example 16 - Minimal Meta-Interpreter
-
-% facts
-mammal(dog).
-mammal(cat).
-
-% simplest meta-interpreter
-prove(H) :- clause(H,B), B=true,
-    write(H), write(" <- "), writeln(B).
