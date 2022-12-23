755811be633498fbd241bc2c936ed2956ec2465c
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-12-23 02:39:18 +0000

    Add files via upload

---
+++ 16 - interpreter_facts.pl
@@ -0,0 +1,9 @@
+% Example 16 - Minimal Meta-Interpreter
+
+% facts
+mammal(dog).
+mammal(cat).
+
+% simplest meta-interpreter
+prove(H) :- clause(H,B), B=true,
+    write(H), write(" <- "), writeln(B).
