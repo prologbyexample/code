1848c6414fbdf6064924d57912dd2a5c51dc9ced
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-12-23 03:55:59 +0000

    Add files via upload

---
+++ 18 - interpreter_multiple_goals.pl
@@ -0,0 +1,13 @@
+% Example 18 - Meta-Interpreter For Multiple Goals
+
+% are two lists the same?
+same([],[]).
+same([H1|T1],[H2|T2]) :- H1=H2, same(T1,T2).
+
+
+% meta-interpreter that covers deduction
+prove(true) :- !.
+prove(A=B) :- !, A=B.
+prove((A,B)):- !, prove(A), prove(B).
+prove(H) :- clause(H,B), prove(B),
+   write(H), write(" <- "), writeln(B).
