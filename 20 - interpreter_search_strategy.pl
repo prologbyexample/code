14668fac303060dd3a648658680a05e5d0270c3b
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-12-26 23:02:59 +0000

    Add files via upload

--- 20 - interpreter_search_strategy.pl
+++ 20 - interpreter_search_strategy.pl
@@ -1,56 +1,44 @@
-% map locations
-% 
-%  a b c
-%  d e f
-%  g h i
-%
-% map paths
-% 
-%  x y x
-%  x x x
-%  x - x
-%
+% Example 20 - Meta-Interpreter For Multiple Goals
 
-node(a). node(b). node(c).
-node(d). node(e). node(f).
-node(g). node(h). node(i).
+% map locations and paths
+% 
+%  a - b - c
+%  |       |
+%  d - e - f
+%  |       |
+%  g   h   i
 
 % additional path (y)
-path(a,b). path(b,c).
-
-path(a,d).
-path(d,e). path(e,f).
-path(d,g). 
+path(a,b). path(b,c). path(a,d).
+path(d,e). path(e,f). path(d,g). 
 path(f,c). path(f,i).
 
 connected(X,Y) :- path(X,Y).
 connected(X,Y) :- path(Y,X).
 
-%journey(X, X, [(X,X)]) :- !.
 journey(X, Y, [(X,Y)]) :- 
-    write("testing -- "), writeln((X,Y)),
+    %write("testing -- "), writeln((X,Y)),
     connected(X,Y).
 journey(X, Y, J2) :-
     [(X,A)|J1]=J2,
-    write("testing .. "), writeln((X,A)),
+    %write("testing .. "), writeln((X,A)),
     connected(X,A),
     journey(A,Y, J1).
 
 
-% meta-interpreter for rules with multiple goals
-%prove(Goal, Max) :- prove(Goal, Max, 0).
-
+% meta-interpreter with fixed depth search
 prove(true, _) :- !.
 prove(A=B, _) :- !, A=B.
 prove(write(X),_) :- !, write(X).
 prove(writeln(X),_) :- !, writeln(X).
-prove((A,B), N):- !, prove(A, N), prove(B, N).
-prove(H, N) :-
-    %write("N = "), writeln(N),
+prove((A,B), Depth):- !, prove(A, Depth), prove(B, Depth).
+prove(H, Depth) :-
+    %write("Depth = "), writeln(N),
     %write(H), write(" <- "), writeln(B),
-    N > 0,
-    N1 is N-1,
-    clause(H,B), prove(B, N1).
+    Depth > 0,
+    NextDepth is Depth-1,
+    clause(H,B), prove(B, NextDepth).
 
-% prove( journey(a,e,X), 5).
+% prove( journey(a,e,X), 4).
+% prove( journey(a,e,X), 6).
 % journey(a,e,X) - doesn't terminate
