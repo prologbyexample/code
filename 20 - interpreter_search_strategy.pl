7374e3a07443680eaa94e76aa42a0a34ef05b65e
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-12-26 01:24:30 +0000

    Add files via upload

--- 20 - interpreter_search_strategy.pl
+++ 20 - interpreter_search_strategy.pl
@@ -19,33 +19,47 @@ node(g). node(h). node(i).
 path(a,b). path(b,c).
 
 path(a,d).
-path(d,g). path(d,e).
-path(e,f). path(f,c).
-path(f,i).
+path(d,e). path(e,f).
+path(d,g). 
+path(f,c). path(f,i).
 
 connected(X,Y) :- path(X,Y).
 connected(X,Y) :- path(Y,X).
 
 %journey(X, X, [(X,X)]) :- !.
-journey(X, Y, [(X,Y)]) :-
-    write("trying .. "), writeln((X,Y)),
-    connected(X,Y), !.
+journey(X, Y, [(X,Y)]) :- 
+    write("testing -- "), writeln((X,Y)),
+    connected(X,Y).
 journey(X, Y, J2) :-
-    connected(X, A),
-    write("trying .. "), writeln((X,A)),
-    J2=[(X,A)|J1], journey(A,Y, J1).
+    [(X,A)|J1]=J2,
+    write("testing .. "), writeln((X,A)),
+    connected(X,A),
+    journey(A,Y, J1).
 
 
 % meta-interpreter for rules with multiple goals
 %prove(Goal, Max) :- prove(Goal, Max, 0).
 
 prove(true, _) :- !.
+prove(A=B, _) :- !, A=B.
 prove(!,_) :- !, ( true ; throw(cut) ).
-prove(A=B,_) :- !, A=B.
 prove((A,B), N):- !, prove(A, N), prove(B, N).
-prove(H, N) :- 
+prove(H, N) :-
     write("N = "), writeln(N),
     %write(H), write(" <- "), writeln(B),
     N > 0,
-    N1 is N -1,
+    N1 is N-1,
     catch((clause(H,B), prove(B, N1)), cut, fail).
+
+
+prove2(true, _) :- !.
+prove2(A=B, _) :- !, A=B.
+prove2(write(X),_) :- !, write(X).
+prove2(writeln(X),_) :- !, writeln(X).
+prove2((A,B), N):- !, prove2(A, N), prove2(B, N).
+prove2(H, N) :-
+    write("N = "), writeln(N),
+    %write(H), write(" <- "), writeln(B),
+    N > 0,
+    N1 is N-1,
+    clause(H,B), prove2(B, N1).
