ca66388b49d65769dd176a462c7e07abed44cf13
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2022-12-29 03:47:45 +0000

    Add files via upload

--- 18 - interpreter_multiple_goals.pl
+++ 18 - interpreter_multiple_goals.pl
@@ -4,9 +4,6 @@
 same([],[]).
 same([H1|T1],[H2|T2]) :- H1=H2, same(T1,T2).
 
-% example of a multi-goal rule
-test(X) :- one(X), two(X), three(X).
-
 % meta-interpreter for rules with multiple goals
 prove(true) :- !.
 prove(A=B) :- !, A=B.
--- 19 - interpreter_cut_negation.pl
+++ 19 - interpreter_cut_negation.pl
@@ -11,13 +11,4 @@ prove((A,B)):- !, prove(A), prove(B).
 prove(H) :- 
     catch((clause(H,B), prove(B)), cut, fail),
     write(H), write(" <- "), writeln(B).
-
-% bad/1 will cause a resource error
-bad(X) :- bad(bad(X)).
-
-% numerology/1 throws an exception if X=13
-numerology(X):- \+ X=13.
-numerology(X):- X=13, throw(unlucky).
-
-% testnumber handles that exception
-testnumber(X) :- catch(numerology(X), unlucky, writeln("unlucky number")).
+    
