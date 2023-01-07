fffd982044ce6f93eb44f88dc79a35b7d8477e23
Prolog By Example <110572081+prologbyexample@users.noreply.github.com>
2023-01-07 21:00:08 +0000

    Add files via upload

--- 20 - interpreter_search_strategy.pl
+++ 20 - interpreter_search_strategy.pl
@@ -8,36 +8,29 @@
 %  |       |
 %  g   h   i
 
+% Example 20 - Meta-Interpreter With Fixed Depth Search
+
+% map locations and links
 link(a,b). link(b,c). link(a,d).
-link(d,e). link(e,f). link(d,g). 
+link(d,e). link(e,f). link(d,g).
 link(f,c). link(f,i).
 
+% two rooms are connected by a link
 connected(X,Y) :- link(X,Y).
 connected(X,Y) :- link(Y,X).
 
-journey(X, Y, [(X,Y)]) :- 
-    %write("testing -- "), writeln((X,Y)),
-    connected(X,Y).
-journey(X, Y, J) :-
-    [(X,A)|J1]=J,
-    %write("testing .. "), writeln((X,A)),
-    connected(X,A),
-    journey(A,Y, J1).
-
+% journey between two locations
+journey(X, Y, [(X,Y)]) :- connected(X,Y).
+journey(X, Y, J) :- [(X,A)|J1]=J, connected(X,A), journey(A,Y, J1).
 
 % meta-interpreter with fixed depth search
 prove(true, _) :- !.
 prove(A=B, _) :- !, A=B.
-prove(write(X),_) :- !, write(X).
-prove(writeln(X),_) :- !, writeln(X).
 prove((A,B), Depth):- !, prove(A, Depth), prove(B, Depth).
 prove(H, Depth) :-
-    %write("Depth = "), writeln(Depth),
     Depth > 0,
     NextDepth is Depth-1,
-    clause(H,B),
-    %write(H), write(" <- "), writeln(B),
-    prove(B, NextDepth).
+    clause(H,B), prove(B, NextDepth).
 
 
 % prove(connected(a,b),2).
